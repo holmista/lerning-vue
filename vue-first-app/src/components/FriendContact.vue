32c7137ea701e8233359f3072a27158122081376
holmista <tornikebuchukuri@redberry.ge>
2022-10-06 01:07:38 +0400

    friend list project with legit vue

---
+++ vue-first-app/src/components/FriendContact.vue
@@ -0,0 +1,39 @@
+<template>
+    <li>
+        <h2>
+            {{friend.name}}
+        </h2>
+        <button @click="setDetailsAreVisible">{{toggleShow}}</button>
+        <ul v-if="detailsAreVisible">
+            <li>{{friend.phone}}</li>
+            <li>{{friend.email}}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+      friend: {
+        id: 1,
+        name: 'tornike',
+        phone: '12345',
+        email: '1@1.com',
+      },
+      detailsAreVisible: true,
+    };
+  },
+  computed: {
+    toggleShow() {
+      return this.detailsAreVisible ? 'hide' : 'Show';
+    },
+  },
+  methods: {
+    setDetailsAreVisible() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+  },
+};
+</script>
