dc842c7531181b526dcdbdbb5787c09f74705229
holmista <tornikebuchukuri@redberry.ge>
2022-10-07 15:18:15 +0400

    modal

--- cmp-adv-01-starting-setup/src/components/ManageGoals.vue
+++ cmp-adv-01-starting-setup/src/components/ManageGoals.vue
@@ -1,4 +1,32 @@
 <template>
-    <h2>managed goals</h2>
-    <input type="text">
+    <h2>manage goals</h2>
+    <input type="text" ref="goal">
+    <button @click="addGoal">add goal</button>
+    <teleport to="body">
+        <error-alert v-if="inputIsInvalid">
+            <h2>
+                input is invalid 
+            </h2>
+            <button @click="inputIsInvalid=false">okay</button>
+        </error-alert>
+    </teleport>
 </template>
+<script>
+    import ErrorAlert from './ErrorAlert.vue';
+    export default {
+        components:{ErrorAlert},
+        data(){
+            return {
+                inputIsInvalid: false,
+            }
+        },
+        methods: {
+            addGoal() {
+                const value = this.$refs.goal.value;
+                if(value===''){
+                    this.inputIsInvalid = true; 
+                }
+            }
+        }
+    }
+</script>
