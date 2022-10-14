4abb33027a2f3ed33c1857fe45b130876511f5a1
holmista <tornikebuchukuri@redberry.ge>
2022-10-14 18:03:42 +0400

    first look at composition api

---
+++ composition-01-starting-setup/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <section class="container">
+    <h2>{{ user.name }}</h2>
+    <h3>{{ user.age }}</h3>
+    <button @click="incrementAge">increment age</button>
+  </section>
+</template>
+
+<script>
+import {ref} from 'vue'
+export default {
+  setup(){
+    const user = ref({
+      name: 'tornike',
+      age: 25
+    })
+    const incrementAge = () => {
+      user.value.age++
+    }
+    return {
+     user,
+     incrementAge
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+.container {
+  margin: 3rem auto;
+  max-width: 30rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 1rem;
+  text-align: center;
+}
+</style>
